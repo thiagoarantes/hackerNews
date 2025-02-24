16e32a00906489cfec4cb801ce4ce6fe5982c656
Thiago Arantes <thiago.arantes@autodesk.com>
2025-02-23 21:24:57 -0500

    Update page titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,17 +9,17 @@ import { PAGE_ROUTES, PAGE_TITLES } from './types';
 export const routes: Routes = [
   {
     path: PAGE_ROUTES.all,
-    title: PAGE_TITLES.all,
+    title: `${PAGE_TITLES.all} - Hacker News`,
     component: AllStoriesComponent,
   },
   {
     path: PAGE_ROUTES.top,
-    title: PAGE_TITLES.top,
+    title: `${PAGE_TITLES.top} - Hacker News`,
     component: TopStoriesComponent,
   },
   {
     path: PAGE_ROUTES.best,
-    title: PAGE_TITLES.best,
+    title: `${PAGE_TITLES.best} - Hacker News`,
     component: BestStoriesComponent,
   },
   { path: '', redirectTo: `/${PAGE_ROUTES.all}`, pathMatch: 'full' },
