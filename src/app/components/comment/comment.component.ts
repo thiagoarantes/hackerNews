73d8f413c41f11d1660dc96767ae7c8525f1f443
Thiago Arantes <thiago.arantes@autodesk.com>
2025-02-25 17:22:05 -0500

    Expanding subcomments

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -3,20 +3,23 @@ import { TimeagoModule } from 'ngx-timeago';
 import { HtmlToPlainTextPipe } from '../../pipes/html-to-plain-text.pipe';
 import { Comment } from '../../types';
 import { HackerNewsService } from '../../services';
+import { PageLinkComponent } from '../page-link/page-link.component';
 
 @Component({
   selector: 'app-comment',
-  imports: [HtmlToPlainTextPipe, TimeagoModule],
+  imports: [HtmlToPlainTextPipe, PageLinkComponent, TimeagoModule],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss',
 })
 export class CommentComponent implements OnInit {
   readonly commentId = input.required<number>();
-  readonly censoredComments = ['undefined', '[flagged]', '[dead]'];
+  readonly censoredComments = [undefined, '[flagged]', '[dead]'];
 
   isLoadingComment = true;
   comment: Comment = {} as Comment;
   isCensoredComment = false;
+
+  protected showReplies = false;
 
   constructor(private readonly dataService: HackerNewsService) {}
 
